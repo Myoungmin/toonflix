47ab369d3eabc57c0bc1c5503fbf1a3aabb12005
Myoungmin <miung222@gmail.com>
2023-02-05 09:30:09 +0900

    appbar 제거, backgroundColor 변경

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello flutter!'),
-        ),
+        backgroundColor: Colors.black,
         body: Center(
           child: Text('Hello world!'),
         ),
